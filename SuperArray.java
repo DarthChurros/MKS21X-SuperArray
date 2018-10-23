8ab338096865a022c861a3e143f894d270018ef0
DarthChurros <tnarayan00@stuy.edu>
2018-10-23 14:21:16 -0400

    added up to add method

---
+++ SuperArray.java
@@ -0,0 +1,27 @@
+public class SuperArray {
+  private String[] data;
+  private int size;
+
+  public SuperArray() {
+    data = new String[10];
+    size = 0;
+  }
+
+  public void clear() {
+    size = 0;
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  public boolean add(String item) {
+    data[size] = item;
+    size++;
+    return true;
+  }
+}
